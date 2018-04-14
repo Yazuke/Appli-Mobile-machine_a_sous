37c8374b67fddf63879b3b7ff3541ade7ec0f99d
Julien Haegman <Haegman.julien@laposte.net>
2018-04-14 01:11:17 -0400

    Merge corrigé - Rouleaux tournent

---
+++ app/src/main/java/com/example/yazuke/applimobilemachine_a_sous/ComboActivity.java
@@ -0,0 +1,15 @@
+package com.example.yazuke.applimobilemachine_a_sous;
+
+import com.example.yazuke.applimobilemachine_a_sous.R;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class ComboActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_combo);
+    }
+}

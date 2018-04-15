43f37f64be504b86b426d1b3fc6108b738f03db7
Julien Haegman <Haegman.julien@laposte.net>
2018-04-15 19:41:16 -0400

    Fermeture de la fenetre

--- app/src/main/java/com/example/yazuke/applimobilemachine_a_sous/ComboActivity.java
+++ app/src/main/java/com/example/yazuke/applimobilemachine_a_sous/ComboActivity.java
@@ -4,6 +4,8 @@ import com.example.yazuke.applimobilemachine_a_sous.R;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 
 public class ComboActivity extends AppCompatActivity {
@@ -11,5 +13,15 @@ public class ComboActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_combo);
+
+
+        //Ferme la fenetre combo
+        Button closeButton = findViewById(R.id.closeButton);
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {finish();}
+        });
+
+
     }
 }
